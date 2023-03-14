b14edcaedd4059d7d622909daa96845d71605b04
Raditya Harya <radityaharya02@gmail.com>
2023-03-14 09:27:54 +0000

    add og-image api

---
+++ lib/spotify-now-playing/dist/service/spotify.d.ts
@@ -0,0 +1,12 @@
+import { SongResult } from '../utils/type';
+export declare class SpotifyService {
+    private accessToken;
+    private clientId;
+    private clientSecret;
+    private refreshToken;
+    constructor(clientId: string, clientSecret: string, refreshToken: string);
+    private hasAccessToken;
+    private setAccessToken;
+    private getAccessToken;
+    getCurrentSong(): Promise<SongResult>;
+}
