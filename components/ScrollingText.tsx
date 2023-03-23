aff7e74da8973c1642fcd2172b85ccee6da8408a
Raditya Harya <radityaharya02@gmail.com>
2023-03-23 17:04:26 +0000

    split now playing, revamp homepage

---
+++ components/ScrollingText.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from "react"
+
+export const ScrollingText = ({ text, ...props }) =>{
+  const [offset, setOffset] = useState(0)
+  const [direction, setDirection] = useState("left")
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const containerWidth = containerRef.current.offsetWidth
+    const textWidth = text.length * 8
+    if (textWidth <= containerWidth) {
+      return
+    }
+
+    const intervalId = setInterval(() => {
+      if (offset <= containerWidth - textWidth) {
+        setDirection("right")
+      } else if (offset >= 0) {
+        setDirection("left")
+      }
+      setOffset(direction === "left" ? offset - 1 : offset + 1)
+    }, 100)
+    return () => clearInterval(intervalId)
+  }, [offset, direction, text.length])
+
+  // update containerRef width on resize
+  useEffect(() => {
+    const handleResize = () => {
+      setOffset(0)
+    }
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return (
+    <div
+      style={{ overflow: "hidden", whiteSpace: "nowrap" }}
+      ref={containerRef}
+    >
+      <div style={{ transform: `translateX(${offset}px)`, width: "min-content" }} {...props}>{text}</div>
+    </div>
+  )
+}
