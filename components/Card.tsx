ecdc86a9415d68f1ea78aaef4f813a46053130df
Raditya Harya <radityaharya02@gmail.com>
2024-03-18 09:04:30 +0700

    lint

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 }
 
 const Card = ({ title, description, imgSrc, href }: CardProps) => (
-  <div className="md max-w-[544px] p-4 md:w-1/2 w-full">
+  <div className="md w-full max-w-[544px] p-4 md:w-1/2">
     <div
       className={`${
         imgSrc && 'h-full'
