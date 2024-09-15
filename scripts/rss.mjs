a2694293196baba45dae9c6ba602b008e55cdd8c
radityaharya <radityaharya02@gmail.com>
2024-09-15 08:29:43 +0000

    Update deps

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,12 +1,11 @@
-import { writeFileSync, mkdirSync } from 'node:fs'
-import path from 'node:path'
-import { slug } from 'github-slugger'
+import { writeFileSync, mkdirSync, readFileSync } from 'node:fs';
+import path from 'node:path';
+import { slug } from 'github-slugger';
 // biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
-import { escape } from 'pliny/utils/htmlEscaper.js'
-import siteMetadata from '../data/siteMetadata.js'
-import tagData from '../app/tag-data.json' assert { type: 'json' }
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
-import { sortPosts } from 'pliny/utils/contentlayer.js'
+import { escape } from 'pliny/utils/htmlEscaper.js';
+import siteMetadata from '../data/siteMetadata.js';
+import { allBlogs } from '../.contentlayer/generated/index.mjs';
+import { sortPosts } from 'pliny/utils/contentlayer.js';
 
 const generateRssItem = (config, post) => `
   <item>
@@ -20,7 +19,7 @@ const generateRssItem = (config, post) => `
     <content:encoded><![CDATA[ ${post.body.raw.replace(/\]\(\/blog\//g, `](${config.siteUrl}/blog/`).replace("\n", "<br>")} ]]></content:encoded>
     <dc:creator>${config.email} (${config.author})</dc:creator>
   </item>
-`
+`;
 
 const generateRss = (config, posts, page = 'feed.xml') => `
   <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
@@ -41,28 +40,32 @@ const generateRss = (config, posts, page = 'feed.xml') => `
       ${posts.map((post) => generateRssItem(config, post)).join('')}
     </channel>
   </rss>
-`
+`;
 
 async function generateRSS(config, allBlogs, page = 'feed.xml') {
-  const publishPosts = allBlogs.filter((post) => post.draft !== true)
+  const loadJSON = (path) => JSON.parse(readFileSync(new URL(path, import.meta.url)));
+
+  const tagData = loadJSON('../app/tag-data.json');
+
+  const publishPosts = allBlogs.filter((post) => post.draft !== true);
   // RSS for blog post
   if (publishPosts.length > 0) {
-    const rss = generateRss(config, sortPosts(publishPosts))
-    writeFileSync(`./public/${page}`, rss)
+    const rss = generateRss(config, sortPosts(publishPosts));
+    writeFileSync(`./public/${page}`, rss);
   }
 
   if (publishPosts.length > 0) {
     for (const tag of Object.keys(tagData)) {
-      const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => slug(t)).includes(tag))
-      const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
-      const rssPath = path.join('public', 'tags', tag)
-      mkdirSync(rssPath, { recursive: true })
-      writeFileSync(path.join(rssPath, page), rss)
+      const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => slug(t)).includes(tag));
+      const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`);
+      const rssPath = path.join('public', 'tags', tag);
+      mkdirSync(rssPath, { recursive: true });
+      writeFileSync(path.join(rssPath, page), rss);
     }
   }
 }
 
 const rss = () => {
-  generateRSS(siteMetadata, allBlogs)
-}
-export default rss
+  generateRSS(siteMetadata, allBlogs);
+};
+export default rss;
