d4827b244276b196462c77a765dcdbe0359fba8a
radityaharya <radityaharya02@gmail.com>
2024-12-12 18:22:39 +0700

    fix: 404 handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,29 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://radityaharya.com',
-  markdown: {
-      shikiConfig: {
-          themes: {
-              light: 'min-dark',
-              dark: 'min-light'
-          }
-      }
-  },
-  integrations: [
-      sitemap(),
-      mdx(),
-      sitemap(),
-      tailwind({
-          applyBaseStyles: false
-      })
-  ],
+    site: 'https://radityaharya.com',
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'min-dark',
+                dark: 'min-light'
+            }
+        }
+    },
+    integrations: [
+        sitemap(),
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ],
 
-  adapter: cloudflare({
-    platformProxy: true
-  })
+    adapter: cloudflare({
+        platformProxy: {
+            configPath: './wrangler.toml'
+        }
+    })
 });
